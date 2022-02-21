b492a6b21bd5bab6910d5daee61379b19481959b
rohitshreenivas <rshreeni@andrew.cmu.edu>
2022-02-21 15:40:57 -0500

    updated delete and retrieve services with logger

--- ms/DeleteServices.java
+++ ms/DeleteServices.java
@@ -12,6 +12,22 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.Naming;
 import java.sql.*;
 import java.io.IOException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DeleteServices extends UnicastRemoteObject implements DeleteServicesAI {
 
@@ -117,6 +133,40 @@ public class DeleteServices extends UnicastRemoteObject implements DeleteService
             ReturnString = e.toString();
         }
 
+        addLog("user", "deleted the order with OrderID: " + orderid);
         return ReturnString;
+    }
+
+    private static void addLog(String username, String logEntry) throws IOException {
+
+        String filePathString = "logs" + File.separator + "logs.txt";
+        File f = new File(filePathString);
+
+        final String dir = System.getProperty("user.dir");
+        Path p = Paths.get(dir + "/logs/logs.txt");
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String s = System.lineSeparator() + dtf.format(now) + " : " + username + " : " + logEntry;
+
+        if(f.exists())
+        {
+            try {
+                Files.write(p, s.getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+        else
+        {
+            f.getParentFile().mkdirs();
+            f.createNewFile();
+
+            try {
+                Files.write(p, s.getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
     }
 }
