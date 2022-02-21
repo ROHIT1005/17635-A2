21d4aa366b8dfce1218e3d0709980056ba383faa
Truong Hoang Phuoc <hoangphuoc25@gmail.com>
2022-02-20 19:09:36 -0500

    implement authentication for ms

---
+++ ms/AuthenticationServicesAI.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+
+public interface AuthenticationServicesAI extends java.rmi.Remote
+{
+	String register(String username, String password) throws RemoteException;
+	String login(String username, String password) throws RemoteException;
+    boolean verify(String token) throws RemoteException;
+}
--- ms/CreateServices.java
+++ ms/CreateServices.java
@@ -19,10 +19,15 @@
 *	= MySQL
 	- orderinfo database 
 ******************************************************************************************************************/
+import java.io.FileReader;
+import java.util.Properties;
 import java.rmi.RemoteException; 
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.Naming;
 import java.sql.*;
+import java.io.IOException;
 
 public class CreateServices extends UnicastRemoteObject implements CreateServicesAI
 { 
@@ -33,9 +38,29 @@ public class CreateServices extends UnicastRemoteObject implements CreateService
     // Set up the orderinfo database credentials
     static final String USER = "root";
     static final String PASS = Configuration.MYSQL_PASSWORD;
-
+    Properties registry = null;
+    
     // Do nothing constructor
-    public CreateServices() throws RemoteException {}
+    public CreateServices() throws IOException {
+        registry = new Properties();
+        registry.load(new FileReader("registry.properties"));
+    }
+
+    private boolean authenticate(String token) {
+        String entry = registry.getProperty("AuthenticationServices");
+        String host = entry.split(":")[0];
+        String port = entry.split(":")[1];
+        try {
+            Registry reg = LocateRegistry.getRegistry(host, Integer.parseInt(port));
+            AuthenticationServicesAI obj = (AuthenticationServicesAI)reg.lookup("AuthenticationServices");
+            boolean response = obj.verify(token);
+            return response;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     // Main service loop
     public static void main(String args[]) 
@@ -57,7 +82,7 @@ public class CreateServices extends UnicastRemoteObject implements CreateService
                 System.out.println("\t" + name);
             }
             // Bind this object instance to the name RetrieveServices in the rmiregistry 
-            // Naming.rebind("//" + Configuration.getRemoteHost() + ":1099/CreateServices", obj); 
+            // Naming.rebind("rmi://" + Configuration.getRemoteHost() + ":1099/CreateServices", obj); 
 
         } catch (Exception e) {
 
@@ -72,8 +97,14 @@ public class CreateServices extends UnicastRemoteObject implements CreateService
 
     // This method add the entry into the ms_orderinfo database
 
-    public String newOrder(String idate, String ifirst, String ilast, String iaddress, String iphone) throws RemoteException
+    public String newOrder(String idate, String ifirst, String ilast, String iaddress, String iphone, String token) throws RemoteException
     {
+        this.authenticate(token); //verify but don't deny access to service
+        
+        boolean isAuthenticated = this.authenticate(token);
+        if (!isAuthenticated) {
+            return "";
+        }
       	// Local declarations
 
         Connection conn = null;		                 // connection to the orderinfo database
